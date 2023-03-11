691b2b0d1496c4a42f80601ec566457822dcec67
Cole Winslow <colethomaswinslow7@gmail.com>
2023-03-10 17:23:17 -0700

    Added Local storage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Switch from "./components/Switch";
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light");
-
+  const [themeMode, setThemeMode] = useState(() => {
+    const storedThemeMode = localStorage.getItem("themeMode");
+    return storedThemeMode !== null ? storedThemeMode : "light";
+  });
   const toggleThemeMode = () => {
     setThemeMode(themeMode === "light" ? "dark" : "light");
+    const newThemeMode = themeMode === "light" ? "dark" : "light";
+    setThemeMode(newThemeMode);
+    localStorage.setItem("themeMode", newThemeMode);
   };
+  useEffect(() => {
+    localStorage.setItem("theme", themeMode);
+    if (themeMode === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [themeMode]);
+
   return (
-    <div className={`App ${themeMode}`}>
+    <div className={`App ${themeMode} dark:bg-gray-900`}>
       {/* Navbar */}
       <header>
         <div className="bg-white dark:bg-gray-900 text-black dark:text-white">
